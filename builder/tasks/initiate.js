42845cb5d2c8a3899633a52dc2ecaa6555c75b7d
Jeremy Bass <jeremybass@cableone.net>
2015-12-10 09:26:41 -0800

    quick correction to the initiate taqsk

--- builder/tasks/initiate.js
+++ builder/tasks/initiate.js
@@ -3,10 +3,8 @@
  * State: bare
 */
 module.exports = function(grunt) {
-	grunt.registerTask('build', 'normalize the build call, ie grunt build named_task', function( ) {
-		var task = 'build_' +  process.argv[3].slice(1,process.argv[3].length);
-		grunt.task.run(task);
-        
+	grunt.registerTask('initiate', 'normalize the build call, ie grunt build named_task', function( ) {
+
         var util = require('util');
         var lastout;
         
@@ -47,6 +45,6 @@ module.exports = function(grunt) {
         
         
 		//done();
-		//grunt.task.current.async();
+		grunt.task.current.async();
 	});
 };
